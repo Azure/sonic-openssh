0df2e6b41de8b35845aa2f297ef8d39b85ba3d47
Colin Watson <cjwatson@debian.org>
2008-04-06 11:14:35 +0000

    Backport from 4.9p1: - Ignore ~/.ssh/rc if a sshd_config ForcedCommand is specified (see http://www.securityfocus.com/bid/28531/info). - Add no-user-rc authorized_keys option to disable execution of ~/.ssh/rc.

--- auth-options.h
+++ auth-options.h
@@ -26,6 +26,7 @@ extern int no_port_forwarding_flag;
 extern int no_agent_forwarding_flag;
 extern int no_x11_forwarding_flag;
 extern int no_pty_flag;
+extern int no_user_rc;
 extern char *forced_command;
 extern struct envstring *custom_environment;
 extern int forced_tun_device;
