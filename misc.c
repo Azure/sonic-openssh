99bd7d0d3fca960b65d84c0f9b928c442db48b0a
Colin Watson <cjwatson@debian.org>
2013-05-07 13:15:13 +0100

    Move platform_sys_dir_uid to misc.c to fix linking following user-group-modes.patch.

--- misc.c
+++ misc.c
@@ -59,6 +59,7 @@
 #include "misc.h"
 #include "log.h"
 #include "ssh.h"
+#include "platform.h"
 
 /* remove newline at end of string */
 char *
@@ -640,6 +641,22 @@ read_keyfile_line(FILE *f, const char *filename, char *buf, size_t bufsz,
 		}
 	}
 	return -1;
+}
+
+/*
+ * return 1 if the specified uid is a uid that may own a system directory
+ * otherwise 0.
+ */
+int
+platform_sys_dir_uid(uid_t uid)
+{
+	if (uid == 0)
+		return 1;
+#ifdef PLATFORM_SYS_DIR_UID
+	if (uid == PLATFORM_SYS_DIR_UID)
+		return 1;
+#endif
+	return 0;
 }
 
 int
