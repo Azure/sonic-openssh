3558be2914c0127489faae40ce2eae66142c3287
Richard Kettlewell <rjk@greenend.org.uk>
2014-02-09 16:09:52 +0000

    Various keepalive extensions
    Add compatibility aliases for ProtocolKeepAlives and SetupTimeOut, supported
    in previous versions of Debian's OpenSSH package but since superseded by
    ServerAliveInterval.  (We're probably stuck with this bit for
    compatibility.)
    
    In batch mode, default ServerAliveInterval to five minutes.
    
    Adjust documentation to match and to give some more advice on use of
    keepalives.
    
    Author: Ian Jackson <ian@chiark.greenend.org.uk>
    Author: Matthew Vernon <matthew@debian.org>
    Author: Colin Watson <cjwatson@debian.org>
    Last-Update: 2020-02-21
    
    Patch-Name: keepalive-extensions.patch

--- readconf.c
+++ readconf.c
@@ -176,6 +176,7 @@ typedef enum {
 	oFingerprintHash, oUpdateHostkeys, oHostbasedKeyTypes,
 	oPubkeyAcceptedKeyTypes, oCASignatureAlgorithms, oProxyJump,
 	oSecurityKeyProvider,
+	oProtocolKeepAlives, oSetupTimeOut,
 	oIgnore, oIgnoredUnknownOption, oDeprecated, oUnsupported
 } OpCodes;
 
@@ -326,6 +327,8 @@ static struct {
 	{ "ignoreunknown", oIgnoreUnknown },
 	{ "proxyjump", oProxyJump },
 	{ "securitykeyprovider", oSecurityKeyProvider },
+	{ "protocolkeepalives", oProtocolKeepAlives },
+	{ "setuptimeout", oSetupTimeOut },
 
 	{ NULL, oBadOption }
 };
@@ -1495,6 +1498,8 @@ parse_keytypes:
 		goto parse_flag;
 
 	case oServerAliveInterval:
+	case oProtocolKeepAlives: /* Debian-specific compatibility alias */
+	case oSetupTimeOut:	  /* Debian-specific compatibility alias */
 		intptr = &options->server_alive_interval;
 		goto parse_time;
 
@@ -2198,8 +2203,13 @@ fill_default_options(Options * options)
 		options->rekey_interval = 0;
 	if (options->verify_host_key_dns == -1)
 		options->verify_host_key_dns = 0;
-	if (options->server_alive_interval == -1)
-		options->server_alive_interval = 0;
+	if (options->server_alive_interval == -1) {
+		/* in batch mode, default is 5mins */
+		if (options->batch_mode == 1)
+			options->server_alive_interval = 300;
+		else
+			options->server_alive_interval = 0;
+	}
 	if (options->server_alive_count_max == -1)
 		options->server_alive_count_max = 3;
 	if (options->control_master == -1)
