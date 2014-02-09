79b8c11f2b7a8258dee45e8f10ee58d0f4e48340
Kees Cook <kees@debian.org>
2014-02-09 16:10:06 +0000

    Add DebianBanner server configuration option
    Setting this to "no" causes sshd to omit the Debian revision from its
    initial protocol handshake, for those scared by package-versioning.patch.
    
    Bug-Debian: http://bugs.debian.org/562048
    Forwarded: not-needed
    Last-Update: 2015-08-19
    
    Patch-Name: debian-banner.patch

--- servconf.c
+++ servconf.c
@@ -169,6 +169,7 @@ initialize_server_options(ServerOptions *options)
 	options->ip_qos_bulk = -1;
 	options->version_addendum = NULL;
 	options->fingerprint_hash = -1;
+	options->debian_banner = -1;
 }
 
 /* Returns 1 if a string option is unset or set to "none" or 0 otherwise. */
@@ -347,6 +348,8 @@ fill_default_server_options(ServerOptions *options)
 		options->fwd_opts.streamlocal_bind_unlink = 0;
 	if (options->fingerprint_hash == -1)
 		options->fingerprint_hash = SSH_FP_HASH_DEFAULT;
+	if (options->debian_banner == -1)
+		options->debian_banner = 1;
 	/* Turn privilege separation on by default */
 	if (use_privsep == -1)
 		use_privsep = PRIVSEP_NOSANDBOX;
@@ -419,6 +422,7 @@ typedef enum {
 	sAuthenticationMethods, sHostKeyAgent, sPermitUserRC,
 	sStreamLocalBindMask, sStreamLocalBindUnlink,
 	sAllowStreamLocalForwarding, sFingerprintHash,
+	sDebianBanner,
 	sDeprecated, sUnsupported
 } ServerOpCodes;
 
@@ -565,6 +569,7 @@ static struct {
 	{ "streamlocalbindunlink", sStreamLocalBindUnlink, SSHCFG_ALL },
 	{ "allowstreamlocalforwarding", sAllowStreamLocalForwarding, SSHCFG_ALL },
 	{ "fingerprinthash", sFingerprintHash, SSHCFG_GLOBAL },
+	{ "debianbanner", sDebianBanner, SSHCFG_GLOBAL },
 	{ NULL, sBadOption, 0 }
 };
 
@@ -1849,6 +1854,10 @@ process_server_config_line(ServerOptions *options, char *line,
 		if (*activep)
 			options->fingerprint_hash = value;
 		break;
+
+	case sDebianBanner:
+		intptr = &options->debian_banner;
+		goto parse_int;
 
 	case sDeprecated:
 		logit("%s line %d: Deprecated option %s",
