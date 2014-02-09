3309e464e5ae6c940ddd584eed4d2d403f4c168c
Colin Watson <cjwatson@debian.org>
2014-02-09 16:09:58 +0000

    Allow harmless group-writability
    Allow secure files (~/.ssh/config, ~/.ssh/authorized_keys, etc.) to be
    group-writable, provided that the group in question contains only the file's
    owner.  Rejected upstream for IMO incorrect reasons (e.g. a misunderstanding
    about the contents of gr->gr_mem).  Given that per-user groups and umask 002
    are the default setup in Debian (for good reasons - this makes operating in
    setgid directories with other groups much easier), we need to permit this by
    default.
    
    Bug: https://bugzilla.mindrot.org/show_bug.cgi?id=1060
    Bug-Debian: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=314347
    Last-Update: 2019-10-09
    
    Patch-Name: user-group-modes.patch

--- readconf.c
+++ readconf.c
@@ -1902,8 +1902,7 @@ read_config_file_depth(const char *filename, struct passwd *pw,
 
 		if (fstat(fileno(f), &sb) == -1)
 			fatal("fstat %s: %s", filename, strerror(errno));
-		if (((sb.st_uid != 0 && sb.st_uid != getuid()) ||
-		    (sb.st_mode & 022) != 0))
+		if (!secure_permissions(&sb, getuid()))
 			fatal("Bad owner or permissions on %s", filename);
 	}
 
