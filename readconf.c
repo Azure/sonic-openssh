02a61bcb045503a5f3f7e274ac1f4524e30f87c8
Richard Kettlewell <rjk@greenend.org.uk>
2014-02-09 16:09:52 +0000

    Various keepalive extensions
    Add compatibility aliases for ProtocolKeepAlives and SetupTimeOut, supported
    in previous versions of Debian's OpenSSH package but since superseded by
    ServerAliveInterval.  (We're probably stuck with this bit for
    compatibility.)
    
    In batch mode, default ServerAliveInterval to five minutes.
    
    Adjust documentation to match and to give some more advice on use of
    keepalives.
    
    Author: Ian Jackson <ian@chiark.greenend.org.uk>
    Author: Matthew Vernon <matthew@debian.org>
    Author: Colin Watson <cjwatson@debian.org>
    Last-Update: 2015-08-19
    
    Patch-Name: keepalive-extensions.patch

--- readconf.c
+++ readconf.c
@@ -159,6 +159,7 @@ typedef enum {
 	oCanonicalizeFallbackLocal, oCanonicalizePermittedCNAMEs,
 	oStreamLocalBindMask, oStreamLocalBindUnlink, oRevokedHostKeys,
 	oFingerprintHash, oUpdateHostkeys, oHostbasedKeyTypes,
+	oProtocolKeepAlives, oSetupTimeOut,
 	oIgnoredUnknownOption, oDeprecated, oUnsupported
 } OpCodes;
 
@@ -288,6 +289,8 @@ static struct {
 	{ "updatehostkeys", oUpdateHostkeys },
 	{ "hostbasedkeytypes", oHostbasedKeyTypes },
 	{ "ignoreunknown", oIgnoreUnknown },
+	{ "protocolkeepalives", oProtocolKeepAlives },
+	{ "setuptimeout", oSetupTimeOut },
 
 	{ NULL, oBadOption }
 };
@@ -1299,6 +1302,8 @@ parse_int:
 		goto parse_flag;
 
 	case oServerAliveInterval:
+	case oProtocolKeepAlives: /* Debian-specific compatibility alias */
+	case oSetupTimeOut:	  /* Debian-specific compatibility alias */
 		intptr = &options->server_alive_interval;
 		goto parse_time;
 
@@ -1858,8 +1863,13 @@ fill_default_options(Options * options)
 		options->rekey_interval = 0;
 	if (options->verify_host_key_dns == -1)
 		options->verify_host_key_dns = 0;
-	if (options->server_alive_interval == -1)
-		options->server_alive_interval = 0;
+	if (options->server_alive_interval == -1) {
+		/* in batch mode, default is 5mins */
+		if (options->batch_mode == 1)
+			options->server_alive_interval = 300;
+		else
+			options->server_alive_interval = 0;
+	}
 	if (options->server_alive_count_max == -1)
 		options->server_alive_count_max = 3;
 	if (options->control_master == -1)
