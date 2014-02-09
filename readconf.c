043f937820e1152df2c8416f37e6c8d923fc1811
Richard Kettlewell <rjk@greenend.org.uk>
2014-02-09 16:09:52 +0000

    Various keepalive extensions
    Add compatibility aliases for ProtocolKeepAlives and SetupTimeOut, supported
    in previous versions of Debian's OpenSSH package but since superseded by
    ServerAliveInterval.  (We're probably stuck with this bit for
    compatibility.)
    
    In batch mode, default ServerAliveInterval to five minutes.
    
    Adjust documentation to match and to give some more advice on use of
    keepalives.
    
    Author: Ian Jackson <ian@chiark.greenend.org.uk>
    Author: Matthew Vernon <matthew@debian.org>
    Author: Colin Watson <cjwatson@debian.org>
    Last-Update: 2013-09-14
    
    Patch-Name: keepalive-extensions.patch

--- readconf.c
+++ readconf.c
@@ -150,6 +150,7 @@ typedef enum {
 	oKexAlgorithms, oIPQoS, oRequestTTY, oIgnoreUnknown, oProxyUseFdpass,
 	oCanonicalDomains, oCanonicalizeHostname, oCanonicalizeMaxDots,
 	oCanonicalizeFallbackLocal, oCanonicalizePermittedCNAMEs,
+	oProtocolKeepAlives, oSetupTimeOut,
 	oIgnoredUnknownOption, oDeprecated, oUnsupported
 } OpCodes;
 
@@ -279,6 +280,8 @@ static struct {
 	{ "canonicalizemaxdots", oCanonicalizeMaxDots },
 	{ "canonicalizepermittedcnames", oCanonicalizePermittedCNAMEs },
 	{ "ignoreunknown", oIgnoreUnknown },
+	{ "protocolkeepalives", oProtocolKeepAlives },
+	{ "setuptimeout", oSetupTimeOut },
 
 	{ NULL, oBadOption }
 };
@@ -1245,6 +1248,8 @@ parse_int:
 		goto parse_flag;
 
 	case oServerAliveInterval:
+	case oProtocolKeepAlives: /* Debian-specific compatibility alias */
+	case oSetupTimeOut:	  /* Debian-specific compatibility alias */
 		intptr = &options->server_alive_interval;
 		goto parse_time;
 
@@ -1724,8 +1729,13 @@ fill_default_options(Options * options)
 		options->rekey_interval = 0;
 	if (options->verify_host_key_dns == -1)
 		options->verify_host_key_dns = 0;
-	if (options->server_alive_interval == -1)
-		options->server_alive_interval = 0;
+	if (options->server_alive_interval == -1) {
+		/* in batch mode, default is 5mins */
+		if (options->batch_mode == 1)
+			options->server_alive_interval = 300;
+		else
+			options->server_alive_interval = 0;
+	}
 	if (options->server_alive_count_max == -1)
 		options->server_alive_count_max = 3;
 	if (options->control_master == -1)
