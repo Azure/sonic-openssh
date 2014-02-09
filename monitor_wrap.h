3131e3bb3c56a6c6ee8cb9d68f542af04cd9e8ff
Manoj Srivastava <srivasta@debian.org>
2014-02-09 16:09:49 +0000

    Handle SELinux authorisation roles
    Rejected upstream due to discomfort with magic usernames; a better approach
    will need an SSH protocol change.  In the meantime, this came from Debian's
    SELinux maintainer, so we'll keep it until we have something better.
    
    Bug: https://bugzilla.mindrot.org/show_bug.cgi?id=1641
    Bug-Debian: http://bugs.debian.org/394795
    Last-Update: 2019-06-05
    
    Patch-Name: selinux-role.patch

--- monitor_wrap.h
+++ monitor_wrap.h
@@ -46,7 +46,8 @@ DH *mm_choose_dh(int, int, int);
 #endif
 int mm_sshkey_sign(struct ssh *, struct sshkey *, u_char **, size_t *,
     const u_char *, size_t, const char *, u_int compat);
-void mm_inform_authserv(char *, char *);
+void mm_inform_authserv(char *, char *, char *);
+void mm_inform_authrole(char *);
 struct passwd *mm_getpwnamallow(struct ssh *, const char *);
 char *mm_auth2_read_banner(void);
 int mm_auth_password(struct ssh *, char *);
