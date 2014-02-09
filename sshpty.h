d55bc528ac450324522f02d90a2bdc4832d1eef8
Manoj Srivastava <srivasta@debian.org>
2014-02-09 16:09:49 +0000

    Handle SELinux authorisation roles
    Rejected upstream due to discomfort with magic usernames; a better approach
    will need an SSH protocol change.  In the meantime, this came from Debian's
    SELinux maintainer, so we'll keep it until we have something better.
    
    Bug: https://bugzilla.mindrot.org/show_bug.cgi?id=1641
    Bug-Debian: http://bugs.debian.org/394795
    Last-Update: 2015-08-19
    
    Patch-Name: selinux-role.patch

--- sshpty.h
+++ sshpty.h
@@ -24,4 +24,4 @@ int	 pty_allocate(int *, int *, char *, size_t);
 void	 pty_release(const char *);
 void	 pty_make_controlling_tty(int *, const char *);
 void	 pty_change_window_size(int, u_int, u_int, u_int, u_int);
-void	 pty_setowner(struct passwd *, const char *);
+void	 pty_setowner(struct passwd *, const char *, const char *);
