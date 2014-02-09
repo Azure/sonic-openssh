bdc94de85ed7dbafb949c239d7c3eff23ea4aa28
Colin Watson <cjwatson@ubuntu.com>
2014-02-09 16:09:50 +0000

    Accept obsolete ssh-vulnkey configuration options
    These options were used as part of Debian's response to CVE-2008-0166.
    Nearly six years later, we no longer need to continue carrying the bulk
    of that patch, but we do need to avoid failing when the associated
    configuration options are still present.
    
    Last-Update: 2014-02-09
    
    Patch-Name: ssh-vulnkey-compat.patch

--- readconf.c
+++ readconf.c
@@ -161,6 +161,7 @@ static struct {
 	{ "passwordauthentication", oPasswordAuthentication },
 	{ "kbdinteractiveauthentication", oKbdInteractiveAuthentication },
 	{ "kbdinteractivedevices", oKbdInteractiveDevices },
+	{ "useblacklistedkeys", oDeprecated },
 	{ "rsaauthentication", oRSAAuthentication },
 	{ "pubkeyauthentication", oPubkeyAuthentication },
 	{ "dsaauthentication", oPubkeyAuthentication },		    /* alias */
