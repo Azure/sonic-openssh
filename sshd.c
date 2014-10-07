c027de5eb3e6cb1718990841c2a9cbc89fd53151
Colin Watson <cjwatson@debian.org>
2014-10-07 13:22:41 +0100

    Restore TCP wrappers support
    Support for TCP wrappers was dropped in OpenSSH 6.7.  See this message
    and thread:
    
      https://lists.mindrot.org/pipermail/openssh-unix-dev/2014-April/032497.html
    
    It is true that this reduces preauth attack surface in sshd.  On the
    other hand, this support seems to be quite widely used, and abruptly
    dropping it (from the perspective of users who don't read
    openssh-unix-dev) could easily cause more serious problems in practice.
    
    It's not entirely clear what the right long-term answer for Debian is,
    but it at least probably doesn't involve dropping this feature shortly
    before a freeze.
    
    Forwarded: not-needed
    Last-Update: 2014-10-07
    
    Patch-Name: restore-tcp-wrappers.patch

--- sshd.c
+++ sshd.c
@@ -129,6 +129,13 @@
 #include <Security/AuthSession.h>
 #endif
 
+#ifdef LIBWRAP
+#include <tcpd.h>
+#include <syslog.h>
+int allow_severity;
+int deny_severity;
+#endif /* LIBWRAP */
+
 #ifndef O_NOCTTY
 #define O_NOCTTY	0
 #endif
@@ -2207,6 +2214,24 @@ main(int ac, char **av)
 #ifdef SSH_AUDIT_EVENTS
 	audit_connection_from(remote_ip, remote_port);
 #endif
+#ifdef LIBWRAP
+	allow_severity = options.log_facility|LOG_INFO;
+	deny_severity = options.log_facility|LOG_WARNING;
+	/* Check whether logins are denied from this host. */
+	if (packet_connection_is_on_socket()) {
+		struct request_info req;
+
+		request_init(&req, RQ_DAEMON, __progname, RQ_FILE, sock_in, 0);
+		fromhost(&req);
+
+		if (!hosts_access(&req)) {
+			debug("Connection refused by tcp wrapper");
+			refuse(&req);
+			/* NOTREACHED */
+			fatal("libwrap refuse returns");
+		}
+	}
+#endif /* LIBWRAP */
 
 	/* Log the connection. */
 	laddr = get_local_ipaddr(sock_in);
